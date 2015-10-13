70d53e5c997353acb4a6de5ad0da4999aa6fa494
andyfoster <andrewfosteris@gmail.com>
2015-10-13 15:10:16 +1300

    React to checking off, incomplete count

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,19 @@ if (Meteor.isClient) {
   Template.body.helpers({
     // get tasks from Mongo Database
     tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // if hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function(){
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -25,6 +37,21 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function(event){
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.body.events({
+    "click .toggle-checked": function(){
+      // Update DB - set checked property to opposite of current
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
     }
   });
 
